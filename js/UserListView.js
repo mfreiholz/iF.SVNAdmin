97995eb72cc71eaa2705aff08a8c49c4062d1531
mfreiholz <manuel@insanefactory.com>
2014-10-23 18:04:46 +0200

    Updates UserListView to use BasicTableView.

--- js/UserListView.js
+++ js/UserListView.js
@@ -1,6 +1,112 @@
 (function (jQ) {
   "use strict";
-  var _providerId = "";
+  brite.registerView("UserListView", {}, {
+
+    create: function (data, config) {
+      var view = this;
+      return jQ("#tmpl-UserListView").render();
+    },
+
+    postDisplay: function (data, config) {
+      var view = this;
+      svnadmin.service.getUserProviders().done(function (resp) {
+        var html = jQ("#tmpl-UserListView-Providers").render({ providers: resp });
+        view.$el.find(".provider-wrapper").html(html);
+        view.showUsers(resp[0].id);
+      });
+    },
+
+    events: {
+
+      "click; .provider-link": function (ev) {
+        var view = this,
+          element = jQ(ev.currentTarget),
+          providerId = element.data("providerid");
+        view.showUsers(providerId);
+      },
+
+      "click; .add-link": function (ev) {
+        var view = this,
+          element = jQ(ev.currentTarget),
+          providerId = element.data("providerid");
+        brite.display("UserAddView", "body", { providerId: providerId, submitted: function () { view.showUsers(providerId); } }, { emptyParent: false });
+      }
+
+    },  // End of events.
+
+    ///////////////////////////////////////////////////////////////////
+    //
+    ///////////////////////////////////////////////////////////////////
+
+    showUsers: function (providerId) {
+      var view = this;
+      var html = jQ("#tmpl-UserListView-UserList").render({ providerId: providerId });
+      view.$el.find(".userlist-wrapper").html(html);
+
+      var options = {
+        showPaging: true,
+        showRowNumber: true,
+        pageSize: 5,
+
+        singleActions: [
+          {
+            id: "info",
+            getName: function (id) { return tr("Info"); },
+            getLink: function (id) { return "#!/users/" + providerId + "/" + id + "/info"; },
+            callback: function (id) { return svnadmin.app.showUserInfoView(providerId, id); }
+          }
+        ],
+
+        multiActions: [
+          {
+            id: "delete",
+            name: tr("Delete"),
+            callback: function (ids) {
+              var promises = [],
+                i = 0;
+              for (i = 0; i < ids.length; ++i) {
+                promises.push(svnadmin.service.deleteUser(providerId, ids[i]));
+              }
+              return jQ.when.apply(null, promises).done(function () {
+                view.showUsers(providerId);
+              });
+            }
+          }
+        ],
+
+        columns: [
+          { id: "", name: tr("Name") },
+          { id: "", name: tr("Login") }
+        ],
+
+        loadMore: function (offset, num) {
+          view.$el.find("li.provider").removeClass("active");
+          var def = new jQuery.Deferred();
+          svnadmin.service.getUsers(providerId, offset, num).done(function (resp) {
+            var obj = {}, i = 0, row = null;
+            obj.hasMore = resp.hasmore;
+            obj.rows = [];
+            for (i = 0; i < resp.users.length; ++i) {
+              row = {};
+              row.id = resp.users[i].id;
+              row.cells = [resp.users[i].displayname, resp.users[i].name];
+              obj.rows.push(row);
+            }
+            def.resolve(obj);
+            view.$el.find("li.provider[data-providerid=" + providerId + "]").addClass("active");
+          }).fail(function () {
+            def.reject();
+          });
+          return def.promise();
+        }
+      };
+      brite.display("BasicTableView", view.$el.find(".table-wrapper"), { options: options });
+    }
+
+  });
+
+}(jQuery));
+/*
 
   function showUsers(providerId, offset, num) {
     if (!providerId) {
@@ -25,39 +131,6 @@
     });
   }
 
-  /**
-   * Register View
-   */
-  brite.registerView("UserListView", {}, {
-
-    create: function (data, config) {
-      return jQ("#tmpl-UserListView").render();
-    },
-
-    postDisplay: function (data, config) {
-      // Load user providers and the users of the first provider.
-      svnadmin.service.getUserProviders().done(function (resp) {
-        var html = jQ("#tmpl-UserListViewProviders").render({ providers: resp, current: resp[0].id });
-        jQ(".provider-selection-wrapper").html(html);
-        showUsers(resp[0].id);
-      });
-    },
-
-    events: {
-
-      "click; .provider-link": function (ev) {
-        var view = this,
-          element = jQ(ev.currentTarget),
-          providerId = element.data("id");
-        showUsers(providerId);
-      },
-      
-      "click; .add-link": function (ev) {
-        var view = this,
-          element = jQ(ev.currentTarget),
-          providerId = element.data("providerid");
-        brite.display("UserAddView", "body", { providerId: providerId, submitted: function () { showUsers(providerId); } }, { emptyParent: false });
-      },
 
       "click; a.user-link": function (ev) {
         var view = this,
@@ -101,8 +174,5 @@
         showUsers(_providerId, offset, num);
         return ev.preventDefault();
       }
-    }
 
-  });
-
-}(jQuery));
+*/
